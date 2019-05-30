ef644adcc29dca8ea20959a1079aefa851575cfb
XiuXiuen114 <fantastic6022@163.com>
2019-05-30 19:27:30 +0800

    注册

--- miniprogram/pages/Login/register.js
+++ miniprogram/pages/Login/register.js
@@ -5,7 +5,12 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    verificationCode: null,
+    telNumber: null,
+    passwd: null,
+    codeInputDisable: false,
+    currentTime: 600,
+    sendCodeBtnText: "获取验证码",
   },
 
   /**
@@ -15,52 +20,103 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  getVerificationCode: function (e) {
+    this.setData({
+      verificationCode: e.detail.value
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  getTelNumber: function (e) {
+    this.setData({
+      telNumber: e.detail.value
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  getPsd: function (e) {
+    this.setData({
+      passwd: e.detail.value
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  getCode: function(){
+    var that = this;
+    if(that.data.telNumber){
+    /*第一步：验证手机号码*/
+    var myreg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1})|(17[0-9]{1}))+\d{8})$/; // 判断手机号码的正则表达式
+   if (that.data.telNumber.length < 11) {
+      wx.showToast({
+        title: '手机号码长度有误!',
+        duration: 2000,
+        icon: 'none'
+      });
+      return;
+    } else if (!myreg.test(that.data.telNumber)) {
+      wx.showToast({
+        title: '手机号码无效!',
+        duration: 2000,
+        icon: 'none'
+      });
+      return;
+    }
+    /*第二步：设置计时器*/
+    // 先禁止获取验证码按钮的点击
+    that.setData({
+      codeInputDisable: true,
+    })
+    // 60s倒计时 setInterval功能用于循环，常常用于播放动画，或者时间显示
+    var currentTime = that.data.currentTime;
+    var interval = setInterval(function () {
+      currentTime--;
+      that.setData({
+        sendCodeBtnText: currentTime + '秒后重新获取'
+      })
+      if (currentTime <= 0) {
+        clearInterval(interval)
+        that.setData({
+          sendCodeBtnText: '获取验证码',
+          currentTime: 60,
+          codeInputDisable: false
+        })
+      }
+    }, 1000);
+    /*第三步：请求验证码接口，并记录服务器返回的验证码用于判断*/
+    that.sendMess();
+    }else{
+      wx.showToast({
+        title: '手机号码不能为空!',
+        duration: 2000,
+        icon: 'none'
+      });
+    }
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  sendMess: function(){
+    var smsvercode = requirePlugin("smsvercode");  //引用短信校验码插件
+    smsvercode.getvercode(this.data.telNumber, function (res) {
+      if (res.errno == "0") {
+      }
+      else {
+        wx.showToast({
+          title: '发送失败',
+        })
+      }
+    });
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  applySubmit: function(){
+    var that = this;
+    var smsvercode = requirePlugin("smsvercode");  //引用短信校验码插件
+    smsvercode.checkvercode(that.data.telNumber, that.data.verificationCode, function (res) {
+      if (res.errno == "0") {
+        wx.showToast({
+          title: '校验成功',
+        })
+      }
+      else {
+        wx.showToast({
+          title: '校验失败',
+        })
+      }
+    });
   }
 })
