8e85971af853bcd26df7cef0a86bcd8e1dec86df
Sabrina <liuyu12ting@163.com>
2019-06-03 11:52:37 +0800

    用户销毁

--- miniprogram/pages/Personal/set/setInfo.js
+++ miniprogram/pages/Personal/set/setInfo.js
@@ -1,30 +1,6 @@
 // miniprogram/pages/Personal/set/setInfo.js
+var app=getApp()
 Page({
-  modalcnt: function () {
-    wx.showModal({
-      title: '注意',
-      content: '账户被系统销毁后您账户的所有数据都会清空，您确定销毁账户吗？',
-      success: function (res) {
-        if (res.confirm) {
-          wx.showToast({
-            title: '账户已销毁',
-          //  duration:1000,
-          })
-          // wx.navigateBack({
-          //   duration:2000,
-          //   url: '../../Home/personal',
-          // })
-          setTimeout(function () {
-            wx.reLaunch({
-              url: '../../Home/personal',
-            })
-          }, 500)
-        } else if (res.cancel) {
-          console.log('用户点击取消')
-        }
-      }
-    })
-  },
   data: {
     menuitems: [
       { text: '个性设置', url: "../personalSet/setpsn", icon: '../../../icon/person.png', tips: '', id: 1, arrows: '../../../icon/youjiantou.png' },
@@ -38,7 +14,76 @@ Page({
   onLoad: function (options) {
 
   },
-
+  onRemove: function () {
+    var id=[];
+    if (app.globalData.userType == '3') {
+      console.log(app.globalData.phone, app.globalData.userType, app.globalData.passwd)
+      const db = wx.cloud.database()
+      db.collection('testing').doc('XPSFhqCEPC1o_GjC').remove({
+        success(res){
+          console.log(res)
+        }
+      })
+      wx.showModal({
+        title: '注意',
+        content: '账户被系统销毁后您账户的所有数据都会清空，您确定销毁账户吗？',
+        success: function (res) {
+          if (res.confirm) {
+            wx.showToast({
+              title: '账户已销毁',
+            })
+            //本地缓存
+            // wx.clearStorageSync()
+            // //全局变量
+            // app.globalData.userInfo=null
+            // app.globalData.userType="1"
+            // app.hasUserInfo=false
+            // app.globalData.userId=null
+            // app.globalData.phone=null
+            // app.globalData.passwd=null
+            // app.globalData.resetPsd=null
+            //数据库记录
+              const db=wx.cloud.database()
+              db.collection('testing').doc('XPSFhqCEPC1o_GjC').remove()
+            
+            // db.collection('comments').where({
+            //   user_id:app.globalData.userId
+            //  }).get({
+            //    success:function(res){
+            //     for(var i=0;i<res.data.length;i++){
+            //       id[i]=(res.data[i]._id);
+            //       console.log(id[i]);
+            //       db.collection('comments').doc('1').remove({
+            //         success:function(res1){
+            //           console.log(res);
+            //           console.log("comments有关记录删除成功");
+            //         },fail:function(err1){
+            //           console.log("删除失败")
+            //         }
+            //       })
+            //      }
+                 
+            //    },fail:function(err){
+            //      console.log(err);
+            //    }
+            //  })
+            setTimeout(function () {
+              wx.reLaunch({
+                url: '../../Home/personal',
+              })
+            }, 500)
+          } else if (res.cancel) {
+            console.log('用户点击取消')
+          }
+        }
+      })
+    } else {
+      wx.showModal({
+        title: '提示',
+        content: '请您登录后再操作',
+      })
+    }
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
