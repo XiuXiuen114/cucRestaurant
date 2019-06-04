93e6510ec0f4d78590659097258f1407896b7261
Sabrina <liuyu12ting@163.com>
2019-06-04 20:14:35 +0800

    修改头像以及昵称

---
+++ miniprogram/pages/Personal/set/setName.js
@@ -0,0 +1,96 @@
+// miniprogram/pages/Personal/set/setName.js
+var app=getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userName:app.globalData.userInfo.nickName
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+  getUsername: function (e) {
+    this.setData({
+      userName: e.detail.value
+    })
+  },
+  
+  submit:function(){
+    var that=this;
+    const db = wx.cloud.database({
+      env: 'minidev-ko6dk'
+    })
+    app.globalData.userInfo.nickName = that.data.userName
+    // db.collection
+    db.collection('users').doc(app.globalData.userId).update({
+      data: {
+        user_name: that.data.userName
+      },
+      success: function (res) {
+        console.log(that.data.userName)
+        wx.showToast({
+          title: '更改成功！',
+        })
+        wx.redirectTo({
+          url: 'userInfo/modify',
+        })
+      }, fail: function (err) {
+        console.log(err);
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
--- miniprogram/pages/Personal/set/userInfo/modify.js
+++ miniprogram/pages/Personal/set/userInfo/modify.js
@@ -1,20 +1,63 @@
 // miniprogram/pages/Personal/set/userInfo/modify.js
+var app = getApp()
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    headPhoto:null,
+    userInfo: null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
+
   onLoad: function (options) {
-
+    this.setData({
+      headPhoto:app.globalData.headPhoto,
+      userInfo: app.globalData.userInfo,
+    })
   },
-
+  set_Name:function(){
+    wx.redirectTo({
+      url: '../setName',
+    })
+  },
+  loadPic:function(){
+    var that = this;
+      wx.chooseImage({
+        count:1,
+        sizeType:['compressed'],
+        sourceType: ['album', 'camera'],
+        success: function(res) {
+          console.log(res.tempFilePaths[0])
+          wx.setStorageSync('headPhoto', res.tempFilePaths[0])
+          app.globalData.headPhoto = res.tempFilePaths[0]
+          that.setData({
+            headPhoto: res.tempFilePaths[0]
+          })
+          getCurrentPages()[getCurrentPages().length - 3].onLoad()
+          const db = wx.cloud.database({
+            env: 'minidev-ko6dk'
+          })
+       // db.collection
+          db.collection('users').doc(app.globalData.userId).update({
+            data: {
+              user_picture: res.tempFilePaths[0],
+            },
+            success:function(res1){
+              console.log(res1)
+              wx.showToast({
+                title: '更换成功！',
+              })
+            },fail:function(err){
+              console.log(err);
+            }
+          })
+        }
+      })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
