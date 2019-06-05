df165dd73d48831f88c10ee52c75a460eea9efb8
jaaagger <46060623+jaaagger@users.noreply.github.com>
2019-06-05 16:32:16 +0800

    mainpage swiper
    finished the swiper model of mainpage; add some data to the ads table in the useless page

--- miniprogram/pages/Home/mainpage.js
+++ miniprogram/pages/Home/mainpage.js
@@ -1,59 +1,18 @@
 // pages/Home/mainpage.js
+var util = require('../../utils/util.js');  
+const app=getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    autoplay:true,
-    interval:2000,
-    duration:500,
-    imgUrls:[], //从数据库取出五个广告图片的url
-    links:[],
-
-    current_time:null,//获取当前时间
-    end_time:null,
-    //dish attributes
-    dish_data:{
-      _id: "3",
-      dish_name: "回锅肉盖饭",
-      dish_picture: null,
-      own_of_res_id: "",
-      dish_price: 13,
-      reserved_number: 102,
-      thumbs_up: 99,
-      thumbs_down: 1,
-      update_time: null
-    },
-    //
-    res_data: {
-      _id: "4",
-      res_name: "麻辣香锅",
-      res_picture: null,
-      res_address: "星光餐厅",
-      res_status: 1,
-      res_phone: "12365",
-      res_rank: null
-    },
-    //
-    comment_data: {
-      _id: "4",
-      comment_content: "有点太咸了！",
-      comment_time: null,
-      dish_id: "2",
-      user_id: "1"
-    },
-    //
-    ad_data: {
-      _id: "1",
-      ad_content: "五折封顶！",
-      ad_picture: this.uploadImage(),
-      ad_price: 300,
-      start_time: null,
-      end_time: null,
-      res_id: 3
-    },
-    
+    //轮播初始化参数
+    indicatorDots: true,
+    autoplay: true,
+    interval: 2000,
+    duration: 1000,
+    ads_list: [],
   },
 
   /**
@@ -61,151 +20,49 @@ Page({
    */
   onLoad: function (options) {
     
-    
-    // this.getUpdateDishes();
-    // this.getAds();
-    // this.getHottestDishes();
-    
-    // this.insertDishData();
-    // this.insertResData();
-    // this.insertCommentData();
-    this.insertAdsData();
-    // this.setTime();
   },
-  setTime:function(){
-    this.current_time = Date.now();
-    var current_time_stamp=Date.getTime(this.current_time);
-    var end_time_stamp=current_time_stamp+30*24*60*60;
-    this.end_time=Date.setTime(end_time_stamp);
-    console.log("current_time,end_time",this.current_time,this.end_time);
+  onShow: function (options) {
+    this.getAds();//广告轮播初始化,获取ads表内在当天范围内的广告，并赋值给ads_list
+    console.log('adslist', this.data.ads_list);
   },
-  //选择上传一张图片，并将图片路径返回
-  uploadImage:function(){
-    wx.chooseImage({
-      count:1,
-      sizeType:['original','compressed'],
-      sourceType:['album','camera'],
-      success: function(res) {
-        console.log(res.tempFilePaths);
-        return res.tempFilePaths[0];
-      },
-    }) // end wx
-  }, // end saveImg
-
   //获取广告数据
-  getAds: function (){
-    db.collection('dishes').where({
-      _id: "1"
+  getAds: function () {
+    const db = wx.cloud.database({
+      env: 'minidev-ko6dk'
+    });
+    const _=db.command
+    var cur=new Date().getTime();//当前时间的时间戳
+    // console.log('cur_time', cur);
+    let that=this;
+    db.collection('ads').where({
+      end_time: _.gte(cur)
     }).get({
       success: function (res) {
-        console.log(res);
+        //res.data为满足条件的json数组
+        that.setData({
+          ads_list:res.data
+        });
       }
     }, {
-        fail: function (err) {
-          console.error(err);
-        }
+        fail: console.error
       })
   },// end getAds
-  //获取今日上新的菜品数据
-  getUpdateDishes: function (){
-    db.collection('dishes').where({
-      _id: "1"
-    }).get({
-      success: function (res) {
-        console.log(res);
-      }
-    }, {
-        fail: function (err) {
-          console.error(err);
-        }
-      })
-  },//end getUpdateDishes
+  //轮播自动切换
+  swiperChange:function(e){
 
-  //获取最热菜品数据
-  getHottestDishes: function (){
-    db.collection('dishes').where({
-      _id: "1"
-    }).get({
-      success: function (res) {
-        console.log(res);
-      }
-    }, {
-        fail: function (err) {
-          console.error(err);
-        }
-      })
-  },//end getHottestDishes
-
-  // 向数据库dishes中插入数据
-  insertDishData: function () {
-    console.log('insertDishData function');
-    //连接数据库
-    const db = wx.cloud.database({
-      env: 'minidev-ko6dk'
-    });
-
-    db.collection('dishes').add({
-      data: this.dish_data
-      }) 
-      .then(res => {
-        console.log(res);
-      })// end add function
-
-  }, // end insertDishData
-  // 向数据库commentses中插入数据
-  insertCommentData: function () {
-    //连接数据库
-    const db = wx.cloud.database({
-      env: 'minidev-ko6dk'
-    });
-
-    db.collection('comments').add({
-      data: comment_data,
-      success: function (res) {
-        console.log(res);
-      }
-    }, {
-        fail: function (err) {
-          console.error(err);
-        }
-      })
-
-  }, // end insertCommentData
-  // 向数据库restaurants中插入数据
-  insertResData: function () {
-    console.log("insertResData function");
-    //连接数据库
-    const db = wx.cloud.database({
-      env: 'minidev-ko6dk'
-    });
-
-     db.collection('restaurants').add({
-      data: res_data
-      }) 
-      .then(res => {
-        console.log(res);
-      })// end add function
-
-  }, // end insertResData
-  // 向数据库ads中插入数据
-  insertAdsData: function () {
-
-    console.log("insertAdsData function");
-    //连接数据库
-    const db = wx.cloud.database({
-      env: 'minidev-ko6dk'
-    });
-    //上传一张广告图片
-    
-    //将菜品数据存储到数据库中
-    db.collection('ads').add({
-      data: ad_data
+  },
+  //轮播选择一张图片
+  swiperClick:function(e){
+    //跳转至该res-id的首页
+    app.globalData.resID = e.target.dataset.link_id;
+    console.log('global resID',app.globalData.resID);
+  },
+  //跳转至useless网页
+  goUseless:function(){
+    wx.navigateTo({
+      url: '/pages/Useless/useless',
     })
-      .then(res => {
-        console.log(res);
-      })// end add function
-
-  }, // end insertAdsData
- 
+  },
   
-})
+ 
+}) // end pages
--- miniprogram/utils/util.js
+++ miniprogram/utils/util.js
@@ -15,7 +15,29 @@ function formatNumber(n) {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
+//将Date类型转换为yyyy-mm-dd
+function convertDate(date) {
+  var yyyy = date.getFullYear().toString();
+  var mm = (date.getMonth() + 1).toString();
+  var dd = date.getDate().toString();
 
+  var mmChars = mm.split('');
+  var ddChars = dd.split('');
+
+  return yyyy + '-' + (mmChars[1] ? mm : "0" + mmChars[0]) + '-' + (ddChars[1] ? dd : "0" + ddChars[0]);
+}
+//将Date类型转换为时间戳
+function dateToStamp(date){
+  return date.getTime(date);
+}
+//判断当前时间是否在start,end之内
+function betweenDate(cur,start,end){
+  if (dateToStamp(cur) >= dateToStamp(start) && dateToStamp(cur) <= dateToStamp(end)){
+    return true;
+  }
+  return false;
+}
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  convertDate:convertDate
 }  
