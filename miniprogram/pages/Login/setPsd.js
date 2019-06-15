3d31c436ca32c9c2a99da60ebfb3a89f4720b280
josephphine <51109571+josephphine@users.noreply.github.com>
2019-06-15 16:44:11 +0800

    样式修改

--- miniprogram/pages/Login/setPsd.js
+++ miniprogram/pages/Login/setPsd.js
@@ -15,7 +15,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    if(app.globalData.passwd && options.flag){
+    if(app.globalData.passwd){
       wx.navigateTo({
         url: 'login',
       })
