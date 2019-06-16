e546beb1e93bfc5093227acd024c4e68df749f02
behands <51108720+behands@users.noreply.github.com>
2019-06-16 21:15:41 +0800

    样式修改

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,8 +1,7 @@
-
 //app.js
 App({
   onLaunch: function () {
-    
+
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
@@ -11,67 +10,49 @@ App({
         env: "minidev-ko6dk"
       })
     }
-    this.getUserInfoIfAuthed();
 
     this.globalData.userInfo = wx.getStorageSync('userInfo')
     this.globalData.userType = wx.getStorageSync('userType')
     this.globalData.headPhoto = wx.getStorageSync('headPhoto')
-    this.globalData.userType = wx.getStorageSync('userType') ? wx.getStorageSync('userType'):1
+    this.globalData.userType = wx.getStorageSync('userType') ? wx.getStorageSync('userType') : 1
     this.globalData.shop_photo = wx.getStorageSync('shop_photo')
-    this.globalData.shop_name= wx.getStorageSync('shop_name')
+    this.globalData.shop_name = wx.getStorageSync('shop_name')
     this.globalData.user_name = wx.getStorageSync('userName')
     this.globalData.userId = wx.getStorageSync('userId')
     this.globalData.phone = wx.getStorageSync('phone')
     this.globalData.headPhoto = wx.getStorageSync('headPhoto')
-    this.globalData.status = wx.getStorageSync('status') ? wx.getStorageSync('status'):1
-
+    this.globalData.status = wx.getStorageSync('status') ? wx.getStorageSync('status') : 1
     this.getUserInfoIfAuthed();
-
-     const db = wx.cloud.database({
+    const db = wx.cloud.database({
       env: 'minidev-ko6dk'
     });
 
-
-    if (this.globalData.phone){
-
-    if (this.globalData.phone && this.globalData.status == '1'){
+    if (this.globalData.phone && this.globalData.status == '1') {
       //连接数据库
       db.collection('users').where({
         user_phone: this.globalData.phone,
       }).get({
         success: function (res) {
           console.log(res);
-          wx.setStorageSync('userId',res.data[0]._id)
+          wx.setStorageSync('userId', res.data[0]._id)
           wx.setStorageSync('headPhoto', res.data[0].user_picture)
           wx.setStorageSync('userName', res.data[0].user_name)
           getCurrentPages().pop().onLoad();
         }
       })
-    } else if (this.globalData.phone && this.globalData.status == '2'){
-
-        db.collection('shop').where({
-          user_phone: this.globalData.phone,
-
-        db.collection('restaurants').where({
-          res_phone: this.globalData.phone,
-
-        }).get({
-          success: function (res) {
-            console.log(res);
-            wx.setStorageSync('userId', res.data[0]._id)
-
-            wx.setStorageSync('shop_photo', res.data[0].res_photo)
-
-            wx.setStorageSync('shop_photo', res.data[0].res_picture)
-
-            wx.setStorageSync('shop_name', res.data[0].res_name)
-            getCurrentPages().pop().onLoad();
-          }
-        })
+    } else if (this.globalData.phone && this.globalData.status == '2') {
+      db.collection('restaurants').where({
+        res_phone: this.globalData.phone,
+      }).get({
+        success: function (res) {
+          console.log(res);
+          wx.setStorageSync('userId', res.data[0]._id)
+          wx.setStorageSync('shop_photo', res.data[0].res_picture)
+          wx.setStorageSync('shop_name', res.data[0].res_name)
+          getCurrentPages().pop().onLoad();
+        }
+      })
     }
-
-  }
-
   },
 
   getUserInfoIfAuthed: function () {
@@ -84,14 +65,10 @@ App({
           wx.getUserInfo({
             success: res1 => {
               console.log("用户信息:");
-
-              console.log(res1.userInfo)
-
-           //   console.log(res1.userInfo)
-
+              //   console.log(res1.userInfo)
               that.globalData.userInfo = res1.userInfo;
               wx.setStorageSync('userInfo', res1.userInfo)
-             // wx.setStorageSync('userType', 1)
+              // wx.setStorageSync('userType', 1)
             }
           })
         }
@@ -113,15 +90,11 @@ App({
     user_name: null,
     resetPsd: false,  //忘记密码相关参数
     appid: 'wxafa0fcf8440c7289',
-    openid:'',
+    openid: '',
     status: '1', //1表示是师生用户，2表示是商家，从而显示不同的主页
-    resID:null,//店家首页
-    dishID:null,//菜品首页
+    resID: null,//店家首页
+    dishID: null,//菜品首页
     rankID: null,//0表示上新、1表示最热、2表示推荐
-
-    cartDishes: null,//购物车中的菜品列表
-
-
   }
 })
 
