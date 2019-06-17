174c417d4398c7999cb05030a875165d69872250
josephphine <51109571+josephphine@users.noreply.github.com>
2019-06-17 18:01:39 +0800

    Revert "respage"
    This reverts commit 930cdfb3474fdaac4d0fd9c78ff76be99112d8ee.

--- miniprogram/pages/Respage/respage.js
+++ miniprogram/pages/Respage/respage.js
@@ -53,23 +53,6 @@ Page({
     //从餐厅页面选择的菜品的id赋值给全局变量，并跳转至菜品页面
     app.globalData.dishID=e.currentTarget.dataset.link_id;
     console.log('clickDish id', app.globalData.dishID);
-  },
-  Clickdish: function (e) {
-    var option = e.currentTarget.dataset.dishid;
-    app.globalData.dishID = option;
-    console.log('Clickdish', app.globalData.dishID);
-  },
-  addToCart: function (e) {
-    let dish = e.currentTarget.dataset.dish;
-    console.log('cartDishes', app.globalData.cartDishes);
-    //判断该菜品是否在购物车里面，已经在购物车里面则不push
-    // console.log(JSON.stringify(app.globalData.cartDishes).indexOf(JSON.stringify(dish)));
-    if (JSON.stringify(app.globalData.cartDishes).indexOf(JSON.stringify(dish)) == -1) {
-      app.globalData.cartDishes.push(dish);
-      console.log('cartDishes', app.globalData.cartDishes);
-    } else {
-      console.log("已经存在");
-    }
-  },
+  }
 
 })
