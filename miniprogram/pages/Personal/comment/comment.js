b82fe15f02698b4b228fc83b66b2792115240cb2
Sabrina <liuyu12ting@163.com>
2019-06-17 21:31:56 +0800

    comment

--- miniprogram/pages/Personal/comment/comment.js
+++ miniprogram/pages/Personal/comment/comment.js
@@ -6,11 +6,66 @@ Page({
    */
   data: {
     dish_info:null,
+    noteMaxLen: 50,
+    noteNowLen: 0,
+    info: '',
+    flag: 0,
     shop_name:null,
     shop_picture:null,
     shop_address:null
   },
+  bindTextAreaChange: function (e) {
+    var that = this
+    var value = e.detail.value,
+      len = parseInt(value.length);
+    if (len > that.data.noteMaxLen)
+      return;
+    that.setData({ info: value, noteNowLen: len })
 
+  },
+  bindSubmit: function () {
+    var that = this;
+    wx.showToast({
+      title: '发布成功',
+      icon: 'success',
+      duration: 1500,
+      mask: false,
+      success: function () {
+        that.setData({ info: '', noteNowLen: 0, flag: 0 })
+      }
+    })
+
+  },
+  changeColor1: function () {
+    var that = this;
+    that.setData({
+      flag: 1
+    });
+  },
+  changeColor2: function () {
+    var that = this;
+    that.setData({
+      flag: 2
+    });
+  },
+  changeColor3: function () {
+    var that = this;
+    that.setData({
+      flag: 3
+    });
+  },
+  changeColor4: function () {
+    var that = this;
+    that.setData({
+      flag: 4
+    });
+  },
+  changeColor5: function () {
+    var that = this;
+    that.setData({
+      flag: 5
+    });
+  },
   /**
    * 生命周期函数--监听页面加载
    */
